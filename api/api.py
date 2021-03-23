5aa8ce45308dd784fcafa5951705651ecc479490
Raymond Gollinger <raymondgollinger@gmail.com>
2021-03-23 16:44:48 -0700

    added ngram range by passing a range value in post

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+#%%
 import flask
 from flask import request, jsonify
 from keybert import KeyBERT
@@ -14,15 +15,21 @@ def api_id():
     else:
         return "Error: something wong"
 
-    results = keybertify(form['data'])
+    if 'range' not in form:
+        range = 1
+    else:
+        range = form['range']
+    results = keybertify(form['data'], range)
 
     return jsonify(results)
 
-def keybertify(data):
+def keybertify(data, range = 1):
     data = data
+    range = int(range)
     model = KeyBERT('distilbert-base-nli-mean-tokens')
-    keywords = model.extract_keywords(data)
+    keywords = model.extract_keywords(data, keyphrase_ngram_range=(1,range))
     return keywords
 
 if __name__ == '__main__':
     app.run()
+# %%
